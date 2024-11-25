7ce66d37ba2f0f245ac3cbda082f02120aafd080
otaviopascutti <otaviopascutti@gmail.com>
2024-11-25 18:33:26 -0300

    problem 0001: final commit

--- 0001_multiples_of_3_or_5.py
+++ 0001_multiples_of_3_or_5.py
@@ -1,8 +1,13 @@
+# initialize sum variable
 sum = 0
-for i in range(10):
+
+# for every i, 0 <= i <= 999
+for i in range(1000):
+    # if i is divisible by 3 or 5, adds i to the sum
     if (i%3 == 0) or (i%5 == 0):
-        print(f"{i} <--")
         sum += i
-    else:
-        print(i)
+
+# prints the final sum
 print(f"Sum: {sum}")
+
+# please note that i can be 0, but in this case, this is not a problem
