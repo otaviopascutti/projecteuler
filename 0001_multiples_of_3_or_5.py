8ba1344904b83d116eece346c3837ec11fbfba25
otaviopascutti <otaviopascutti@gmail.com>
2024-11-26 19:31:24 -0300

    doc edit

--- 0001_multiples_of_3_or_5.py
+++ 0001_multiples_of_3_or_5.py
@@ -1,13 +1,11 @@
-# initialize sum variable
+#variable sor the sum
 sum = 0
 
-# for every i, 0 <= i <= 999
-for i in range(1000):
+# it will loop through i=1 to i=999
+for i in range(1, 1000):
     # if i is divisible by 3 or 5, adds i to the sum
     if (i%3 == 0) or (i%5 == 0):
         sum += i
 
 # prints the final sum
 print(f"Sum: {sum}")
-
-# please note that i can be 0, but in this case, this is not a problem
--- 0002_even_fibonacci_numbers.py
+++ 0002_even_fibonacci_numbers.py
@@ -1,18 +1,19 @@
-# initialize a(n)
+#variable for current term
 an = 1
-# initialize a(n-1)
+#variable for a(n-1) term, i'm also starting it with 1 because you'll see it will work out just fine
 an_minus_one = 1
-# max value for fibonacci number
+#max is the greatest number i'll allow for the sequence
 max = 4000000
-# initialize the sum
+#sum variable
 sum = 0
 
 # while the current fibonacci number do not exceed max
 while an <= max:
-    # if even, add it to the sum
+    # if current term is even, add it to the sum
     if (an % 2) == 0:
         sum += an
-    # recalculate for next iteration
+    # recalculate current term for next iteration
+    # I chose to do it in three lines like this because it feels more readable to me
     aux = an
     an += an_minus_one
     an_minus_one = aux
